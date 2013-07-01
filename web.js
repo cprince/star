43bc42f34be73db34e28a8619142503ae42a2984
Colin Prince <col@colinprince.com>
2013-07-01 01:47:24 -0400

    start request

--- web.js
+++ web.js
@@ -1,10 +1,30 @@
 var express = require("express");
+var request = require("request");
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+  res.send(codwallup());
 });
+
+var codwallup = function () {
+	var forecastbase = "https://api.forecast.io/forecast/";
+	var key = "91ac025a6fe778dbe3a41cf7748b55d1";
+	// var opts = "/43.654,-79.423,1370495580?units=ca";
+	var opts = "/43.654,-79.423?units=ca";
+
+	var requrl = forecastbase + key + opts;
+
+    console.log("requrl",requrl);
+
+	return request(requrl, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	  	var details = JSON.parse(body);
+	    console.log(details.longitude);
+	  }
+	})
+};
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
