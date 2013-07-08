b73efbb4f3402183f8cf9eb31058a2a59355cd66
Colin Prince <col@colinprince.com>
2013-07-07 22:09:55 -0400

    try q promises

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var request = require("request");
+var q = require("q");
 var app = express();
 app.use(express.logger());
 var exec = require('child_process').exec,
@@ -7,7 +8,7 @@ var exec = require('child_process').exec,
 
 var checkAndSend = function () {
     console.log("in sendMail");
-    child = exec('echo "This will be an alert" | mail -s "Hello nodejs" yl1984108@gmail.com',
+    child = exec('echo "This will be an alert" | mail -s "Hello nodejs" col@colinprince.com',
       function (error, stdout, stderr) {
         console.log('stdout: ' + stdout);
         console.log('stderr: ' + stderr);
@@ -33,6 +34,45 @@ app.get('/rain/:lat,:lng', function(req, response) {
 app.get('/', function(req, response) {
     response.send('<p>Welcome to the rain predictor</p><p>example:</p><p><a href="/rain/44,-78">/rain/:lat,:lng</a></p>');
 });
+
+var checkrain = function (lat, lng) {
+    var deferred = q.defer();
+    var forecastbase = "https://api.forecast.io/forecast/";
+    var key = "91ac025a6fe778dbe3a41cf7748b55d1";
+    var opts = "/" + lat + "," + lng + "?units=ca";
+
+    var requrl = forecastbase + key + opts;
+    request(requrl, function (error, responseRR, body) {
+      if (!error && responseRR.statusCode == 200) {
+        var details = JSON.parse(body);
+        var raining = false;
+        if (details.currently.precipIntensity > 0) {
+            raining = true;
+        }
+        deferred.resolve({
+                        latitude: details.latitude,
+                        longitude: details.longitude,
+                        raining: raining
+                      });
+      } else {
+        deferred.reject("oops something didn't work");
+      }
+    })
+
+    return deferred.promise;
+
+};
+
+checkrain(43.654,-79.423).then(function(value){
+    console.log("woo",value);
+});
+
+console.log("between");
+
+checkrain(0,0).then(function(value){
+    console.log("woo",value);
+});
+
 
 var getForecast = function (response, lat, lng) {
     var forecastbase = "https://api.forecast.io/forecast/";
